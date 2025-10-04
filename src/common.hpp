992ffc2168f7c3e8082c55d118f5210f32c0c42d
yogesh952003@gmail.com <yogesh952003@gmail.com>
2025-10-05 00:36:14 +0530

    Add more address to common.hpp

--- src/common.hpp
+++ src/common.hpp
@@ -26,3 +26,11 @@ constexpr std::uint16_t OAM_SIZE{0xA0};
 constexpr std::uint16_t OAM_START_ADDRESS{0xFE00};
 constexpr std::uint16_t SCY_REGISTER_ADDRESS{0xFF42};
 constexpr std::uint16_t SCX_REGISTER_ADDRESS{0xFF43};
+
+constexpr std::uint16_t BG_WIN_TILEMAP_ADDRESS0{0x9800};
+constexpr std::uint16_t BG_WIN_TILEMAP_ADDRESS1{0x9C00};
+constexpr std::uint16_t BG_WIN_TILEDATA_ADDRESS0{0x9000}; // This address mode uses 0x9000 as the base address and offset are signed
+constexpr std::uint16_t BG_WIN_TILEDATA_ADDRESS1{0x8000}; // This addresses mode uses 0x8000 as the base address and offset are unsigned
+constexpr unsigned int BG_WIN_TILEMAP_ROW_SIZE{32};
+
+constexpr unsigned int TILE_DATA_SIZE{16}; // each tile takes 16 bytes
