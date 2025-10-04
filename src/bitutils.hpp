c8a28b2cdfbf26aabe76d2335c9f086ceb723874
yogesh952003@gmail.com <yogesh952003@gmail.com>
2025-10-04 14:55:51 +0530

    Update constaints on bituitls

--- src/bitutils.hpp
+++ src/bitutils.hpp
@@ -7,14 +7,14 @@ namespace BitUtils
 {
 
 template <unsigned int N, typename T>
-  requires std::unsigned_integral<T>
+  requires std::unsigned_integral<T> || std::convertible_to<T, unsigned int>
 constexpr T Set(T &&data)
 {
   return (data | (1U << N));
 }
 
 template <int N, typename T>
-  requires std::unsigned_integral<T>
+  requires std::unsigned_integral<T> || std::convertible_to<T, unsigned int>
 constexpr void Set(T &data)
 {
   data = Set<N>(std::move(data));
@@ -25,14 +25,14 @@ static_assert(Set<1>(0U) == 2U, "Set Not Working");
 static_assert(Set<2>(0U) == 4U, "Set Not Working");
 
 template <unsigned int N, typename T>
-  requires std::unsigned_integral<T>
+  requires std::unsigned_integral<T> || std::convertible_to<T, unsigned int>
 constexpr T Unset(T &&data)
 {
   return (data & ~(1U << N));
 }
 
 template <unsigned int N, typename T>
-  requires std::unsigned_integral<T>
+  requires std::unsigned_integral<T> || std::convertible_to<T, unsigned int>
 constexpr void Unset(T &data)
 {
   data = Unset(std::move(data));
@@ -43,7 +43,7 @@ static_assert(Unset<1>(6U) == 4U, "Unset not working");
 static_assert(Unset<3>(8U) == 0U, "Unset not working");
 
 template <unsigned int N, typename T>
-  requires std::unsigned_integral<T>
+  requires std::unsigned_integral<T> || std::convertible_to<T, unsigned int>
 constexpr bool Test(T &&data)
 {
   return ((data >> N) & 1U) == 1U;
