b8377562e7b704c742c971ccc79b33562938c8bc
Yogesh Rathod <yogesh952003@gmail.com>
2026-04-19 21:27:20 +0530

    format: bootrom.cpp

--- src/bootrom.cpp
+++ src/bootrom.cpp
@@ -1,39 +1,43 @@
 #include "bootrom.hpp"
 
+#include <spdlog/spdlog.h>
+
 #include <format>
 #include <fstream>
 #include <stdexcept>
 #include <string>
-#include <spdlog/spdlog.h>
 
 #include "common.hpp"
 #include "logmanager.hpp"
 
-BootRom::BootRom()
-  : _enabled{true}
+BootRom::BootRom() : _enabled{true}
 {
   _logger = LogManager::GetLogger("BootRom");
 }
 
 bool BootRom::Contains(std::uint16_t addr) const
 {
-  return _enabled && (FileMemoryRange::Contains(addr) || addr == BOOTROM_ENABLE_ADDRESS);
+  return _enabled
+         && (FileMemoryRange::Contains(addr) || addr == BOOTROM_ENABLE_ADDRESS);
 }
 
-void BootRom::Write(std::uint16_t addr, std::uint8_t data)
+void BootRom::Write(std::uint16_t addr, [[maybe_unused]] std::uint8_t data)
 {
   // BootRom writes to address 0xFF50 at the end to disable itself
   if (addr == BOOTROM_ENABLE_ADDRESS)
   {
-    LOG_INFO(_logger, "Detected Write to address: {:#04X}, Disabling BootRom", BOOTROM_ENABLE_ADDRESS);
+    LOG_INFO(_logger, "Detected Write to address: {:#04X}, Disabling BootRom",
+        BOOTROM_ENABLE_ADDRESS);
     _enabled = false;
     return;
   }
   // writes to bootRom are ignored
-  LOG_WARN(_logger, "Ignoring write to BootRom address: {:#06X}, data: {:#04X}\n", addr, data);
+  LOG_WARN(_logger,
+      "Ignoring write to BootRom address: {:#06X}, data: {:#04X}\n", addr,
+      data);
 }
 
-void BootRom::Load(const std::string& filePath)
+void BootRom::Load(const std::string &filePath)
 {
   FileMemoryRange::Load(filePath, BootRomOffset);
 }
